6696c114cb862b3907e0800370f952f95cf64931
MahmoudElmahalawy <mahmoud.hamdy.elmahalawy@gmail.com>
2023-04-01 16:10:33 +0200

    Created complaint box

---
+++ src/components/common/ComplaintBox.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import useTranslation from "next-translate/useTranslation";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Image from "next/image";
+import { theme } from "@/styles/mui/theme";
+
+export default function ComplaintBox() {
+	const { t, lang } = useTranslation("common");
+
+	return (
+		<Box dir={lang === "ar" ? "rtl" : "ltr"} sx={{ mt: 2, marginInlineStart: { lg: 2 } }}>
+			<Box
+				sx={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "space-around",
+					px: 2,
+					py: 4,
+					border: "1px solid",
+					borderColor: theme.palette.divider,
+					borderRadius: 2,
+				}}
+			>
+				<Box sx={{ marginInlineEnd: 1.5 }}>
+					<Image
+						src="./static/images/illustrations/question-mark-illustration.svg"
+						alt="Complaints illustration"
+						width={100}
+						height={60}
+					/>
+				</Box>
+				<Box sx={{ display: "flex", flexDirection: "column", justifyContent: "center", gap: 2 }}>
+					<Typography component="div">{t("do_you_have_a_problem_with_shipment")}</Typography>
+					<Button variant="contained" sx={{ boxShadow: "none", borderRadius: 3 }}>
+						{t("submit_complaint")}
+					</Button>
+				</Box>
+			</Box>
+		</Box>
+	);
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,12 @@ import Grid from "@mui/material/Grid";
 import ShipmentInfoHeader from "@/components/tracking-shipments/ShipmentInfoHeader";
 import ShipmentDetailsTable from "@/components/tracking-shipments/ShipmentDetailsTable";
 import DeliveryAddressBox from "@/components/tracking-shipments/DeliveryAddressBox";
+import ComplaintBox from "@/components/common/ComplaintBox";
+import useTranslation from "next-translate/useTranslation";
 
 export default function Home() {
+	const { lang } = useTranslation();
+
 	return (
 		<>
 			<Head>
@@ -15,7 +19,7 @@ export default function Home() {
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<Container dir="rtl">
+			<Container dir={lang === "ar" ? "rtl" : "ltr"}>
 				<Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", py: 6 }}>
 					<ShipmentInfoHeader />
 					<Grid container maxWidth={1000}>
@@ -24,6 +28,7 @@ export default function Home() {
 						</Grid>
 						<Grid item xs={12} lg={5}>
 							<DeliveryAddressBox />
+							<ComplaintBox />
 						</Grid>
 					</Grid>
 				</Box>
