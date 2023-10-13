8fd3f7e11aba1ea22281a36d11d1f885403a4db6
MahmoudElmahalawy <mahmoud.hamdy.elmahalawy@gmail.com>
2023-10-13 15:16:59 +0300

    Test disabling static exports

--- next.config.js
+++ next.config.js
@@ -3,12 +3,6 @@ const nextTranslate = require("next-translate-plugin");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	/**
-	 * Enable static exports for the App Router.
-	 *
-	 * @see https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
-	 */
-	output: "export",
 
 	/**
 	 * Set base path. This is usually the slug of your repository.
